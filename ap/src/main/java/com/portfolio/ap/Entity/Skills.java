f8ca793f1d352c20b0c90ca1fad4ec0f54f346db
ariana-perez <ariana_perez0402@hotmail.com>
2022-08-24 23:10:43 -0300

    Back del componente HyS

---
+++ ap/src/main/java/com/portfolio/ap/Entity/Skills.java
@@ -0,0 +1,54 @@
+
+package com.portfolio.ap.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Skills {
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id;
+    private String nombreS;
+    private int descripcionS;
+    
+    
+
+    public Skills() {
+    }
+
+    public Skills(String nombreS, int descripcionS) {
+        this.nombreS = nombreS;
+        this.descripcionS = descripcionS;
+    }
+    
+  
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreS() {
+        return nombreS;
+    }
+
+    public void setNombreS(String nombreS) {
+        this.nombreS = nombreS;
+    }
+
+    public int getDescripcionS() {
+        return descripcionS;
+    }
+
+    public void setDescripcionS(int descripcionS) {
+        this.descripcionS = descripcionS;
+    }
+    
+}
